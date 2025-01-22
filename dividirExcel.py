e8a01b716b6d3d906a32c6c034d41d34801e8208
Javier Cuasapud <edwarcuasapud@gmail.com>
2025-01-21 20:16:48 -0500

    Listo datos para prueba

--- dividirExcel.py
+++ dividirExcel.py
@@ -3,10 +3,15 @@ import os
 
 # Cargar el archivo CSV
 archivo = "delitos_bucaramanga_202501202158.csv"  # Cambia esto por la ruta a tu archivo original
+# Cargar archivo Excel asegurando que se lean todas las columnas
+df = pd.read_csv('delitos_bucaramanga_202501202158.csv', usecols=None, header=0)  # Usa la primera fila como encabezado
+
+# Mostrar las primeras filas para verificar si todas las columnas fueron cargadas
+print(df.head())
 df = pd.read_csv(archivo)
 
 # Seleccionar 20 filas aleatorias
-muestra = df.sample(n=100)
+muestra = df.sample(n=15)
 
 # Definir la ruta a la carpeta donde quieres guardar el archivo
 directorio_destino = r"C:\Users\Hewlett Packard\Desktop\Nueva carpeta\Modelo-simulacion-delitos-"  # Ruta deseada
